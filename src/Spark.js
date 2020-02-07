5039630c54f6a19bce742c09cdafd58a8f814bfc
houlinjiang <houlinjiang@jd.com>
2020-02-07 11:09:00 +0800

    fix spark not work

--- src/Spark.js
+++ src/Spark.js
@@ -12,16 +12,10 @@ class Spark extends Arc {
     this.trailAngle = this.startAngle;
     this.arcAngle = this.startAngle;
     // 是否有阴影
-    // this.animateBlur = true;
 
-    // 保证Spark的弧度不超过Math.PI
-    if (this.startAngle * this.endAngle < 0) {
-      if (this.startAngle < 0) {
-        this.startAngle += Math.PI * 2;
-        this.endAngle += Math.PI * 2;
-      } else {
-        this.endAngle += Math.PI * 2;
-      }
+    // this.animateBlur = true;
+    if (Math.abs(this.startAngle - this.endAngle) > Math.PI) {
+      this.endAngle += Math.PI * 2;
     }
 
     this.marker = new Marker({
